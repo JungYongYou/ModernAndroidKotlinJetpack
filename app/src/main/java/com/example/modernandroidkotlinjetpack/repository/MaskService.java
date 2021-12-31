4117808079a39b3f4535dd38466d7c63f1672872
LUKARYU <fbfmzk1990>
2022-01-01 02:41:04 +0900

    API 호출 부분 오류 처리

--- app/src/main/java/com/example/modernandroidkotlinjetpack/repository/MaskService.java
+++ app/src/main/java/com/example/modernandroidkotlinjetpack/repository/MaskService.java
@@ -8,6 +8,6 @@ import retrofit2.http.GET;
 public interface MaskService {
     String BASE_URL = "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/";
 
-    @GET("sample.json/json")
+    @GET("sample.json")
     Call<StoreInfo> fetchStoreInfo();
 }
